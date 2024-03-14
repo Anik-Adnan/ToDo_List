ddd9fe1806cdbc1817b4a7d8e3f78f1390c558cb
Anik Adnan <anikadnanict17@gmail.com>
2024-03-15 03:05:29 +0600

    task set up done

--- todo_list/lib/screens/HomePage.dart
+++ todo_list/lib/screens/HomePage.dart
@@ -1,15 +1,25 @@
-
 import 'package:flutter/material.dart';
+import 'package:timezone/data/latest.dart' as tzdata;
+import 'package:timezone/timezone.dart' as tz;
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _dateTimeController = TextEditingController();
   List<String> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    tzdata.initializeTimeZones();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,44 +42,42 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: SingleChildScrollView(
-          child: Container(
-            // color: Colors.blue,
-            padding: const EdgeInsets.all(8.0),
-            child: ListView.separated(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: tasks.length,
-              separatorBuilder: (context, index) => SizedBox(height: 10.0),
-              itemBuilder: (context, index) {
-                return Material(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.separated(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: tasks.length,
+            separatorBuilder: (context, index) => SizedBox(height: 10.0),
+            itemBuilder: (context, index) {
+              return Material(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 2.0,
+                color: index % 2 == 0 ? Colors.cyan.shade700 : Colors.cyan.shade500,
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  title: Text(tasks[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.black87,
+                    onPressed: () {
+                      setState(() {
+                        tasks.removeAt(index);
+                      });
+                    },
                   ),
-                  elevation: 2.0,
-                  // color: Colors.cyan,
-                  color: index % 2 == 0 ? Colors.cyan.shade700 : Colors.cyan.shade500,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                    title: Text(tasks[index]),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Colors.black87,
-                      onPressed: () {
-                        setState(() {
-                          tasks.removeAt(index);
-                        });
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
+                ),
+              );
+            },
           ),
         ),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         onPressed: () {
-          _addTask();
+          _showAddTaskDialog(context);
         },
         child: const Icon(
           Icons.add,
@@ -79,9 +87,98 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _addTask() {
-    setState(() {
-      tasks.add("Task ${tasks.length + 1}");
-    });
+  Future<void> _showAddTaskDialog(BuildContext context) async {
+    DateTime? pickedDate;
+    TimeOfDay? pickedTime;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Add Task"),
+          content: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(labelText: "Task Description"),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter a description";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    controller: _dateTimeController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Notification Date & Time",
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.calendar_today),
+                        onPressed: () async {
+                          final pickedDateTemp = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime(2100),
+                          );
+
+                          if (pickedDateTemp != null) {
+                            final pickedTimeTemp = await showTimePicker(
+                              context: context,
+                              initialTime: TimeOfDay.now(),
+                            );
+
+                            if (pickedTimeTemp != null) {
+                              setState(() {
+                                pickedDate = pickedDateTemp;
+                                pickedTime = pickedTimeTemp;
+                                _dateTimeController.text = "${pickedDateTemp.toIso8601String().substring(0, 10)} ${pickedTimeTemp.format(context)}";
+                              });
+                            }
+                          }
+                        },
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please select date and time";
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  setState(() {
+                    tasks.add(_descriptionController.text);
+                    _descriptionController.clear();
+                    _dateTimeController.clear();
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
